1ad0f89b7155b06fb10abd5e12ac14102d747d34
Huan Yuan <yuanhuan_2005@126.com>
2015-05-05 20:07:10 +0800

    add functions and control code

--- my/my_function.py
+++ my/my_function.py
@@ -22,3 +22,20 @@ def fib1(n):
 
 fib(10000)
 fib1(10000)
+
+
+def ask_ok(prompt, retries=4, complaint='Yes or no, please!'):
+    while True:
+        ok = raw_input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise IOError('refused user')
+        print complaint
+
+
+ask_ok("Do you really want to quit? [yes/no]")
+
