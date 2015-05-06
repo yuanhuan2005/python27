6acfa632d039dcff670e616a15927a6b1d1c2732
Huan Yuan <yuanhuan_2005@126.com>
2015-05-06 19:47:55 +0800

    add time code

---
+++ my/my_calc_days.py
@@ -0,0 +1,3 @@
+from datetime import date
+
+print str((date.today() - date(2009, 11, 30)).days) + " days"
--- my/my_time.py
+++ my/my_time.py
@@ -8,10 +8,15 @@ print time.localtime(time.time())
 print time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(time.time()))
 
 # interval days between 2 dates
-start_date = datetime.datetime(2009, 11, 30)
+start_date = datetime.datetime(1985, 8, 5)
 current_date = datetime.datetime.now()
 print str((current_date - start_date).days) + " days"
 
 # get a date after some days
 future_date = start_date + datetime.timedelta(days=2000)
 print future_date.date()
+
+# another way to get interval days between 2 dates
+from datetime import date
+
+print str((date.today() - date(2009, 11, 30)).days) + " days"
